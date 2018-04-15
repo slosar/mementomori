7d8e949e835c47d637bd878725b723f431296782
Anze Slosar <slosar@gmail.com>
2018-04-15 08:25:34 -0400

    decided we don't need death as I never look at it

--- memento.c
+++ memento.c
@@ -1,5 +1,7 @@
 /* defines */
 #define GLOB_STR "/home/anze/spool/email/*content"
+#define uncomment_DODEATH /* if you want to also get diff to TIMEDEATH  */
+
 /* use gettimes.py for these */
 #define TIMEBIRTH 249109200.0
 #define TIMEDEATH 2805253200.0
@@ -13,10 +15,13 @@ int main() {
 
 time_t secnow = time(NULL);
 int dayspassed = (int)((secnow-TIMEBIRTH)/(24*3600));
-int daystogo = (int)((TIMEDEATH-secnow)/(24*3600));
-
-printf ("[%i <-> %i",dayspassed, daystogo);
-
+#ifdef DODEATH
+ int daystogo = (int)((TIMEDEATH-secnow)/(24*3600));
+ printf ("[%i <-> %i",dayspassed, daystogo); */
+#else
+printf ("[%i",dayspassed); 
+#endif
+ 
 glob_t p;
 int res=glob(GLOB_STR, GLOB_NOSORT,
 	       NULL, &p);
